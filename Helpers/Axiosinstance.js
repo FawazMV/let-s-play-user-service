5fb7ef7b1f53c1da2ec43f3d93dc696fdd71a90a
Muhammed Fawaz MV <mvfawazmfz@gmail.com>
2023-04-10 12:29:55 +0530

    axios instance renewed

--- Helpers/Axiosinstance.js
+++ Helpers/Axiosinstance.js
@@ -1,13 +1,13 @@
 import Axios from 'axios'
 export const Axiosbooking = Axios.create({
-    baseURL: 'http://localhost:4321'
+    baseURL: 'https://let-s-play-booking-service.onrender.com'
 })
 
 export const Axiosturf = Axios.create({
-    baseURL: 'http://localhost:8888'
+    baseURL: 'https://let-s-play-turf-service.onrender.com'
 })
 
 export const AxiosReview = Axios.create({
-    baseURL: 'http://localhost:9999'
+    baseURL: 'https://let-s-play-review-service.onrender.com'
 })
 
